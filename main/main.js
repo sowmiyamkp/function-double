30325d2a57bba9e40e28c1f64af35f89628d28dc
sowmiyamkp <sowmiyamkp1305@gmail.com>
2023-03-25 20:51:18 -0700

    add function-double

---
+++ main/main.js
@@ -0,0 +1,27 @@
+    //syntax in for
+	   
+for(i=0;i<=4;i++)
+	{
+		console.log(i);
+	}
+	
+	//function
+	
+	function name()
+	{
+		console.log("hello");
+	}
+	name();
+	name();
+	
+	   // function double
+	   
+	function welcome(a)
+	{
+		console.log(a);
+	}
+	welcome("hello");
+	welcome("hii")
+	
+	
+	
